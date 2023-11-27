15dae6d046536490a4e383db42b0ff277dada74a
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-11-27 14:10:45 -0300

    Adicionado plugin para trabalhar com variáveis de ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:fifa_world_cup_app/app/core/config/env/env.dart';
 import 'package:fifa_world_cup_app/app/fifa_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Env.i.load();
+
   runApp(const FifaApp());
 }

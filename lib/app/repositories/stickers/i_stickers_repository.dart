8c163f9a02418994878d8751e3edd2928ccb23ee
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-11-30 15:23:24 -0300

    Métodos no repositório para Criar e Buscar figurinhas

--- lib/app/repositories/stickers/i_stickers_repository.dart
+++ lib/app/repositories/stickers/i_stickers_repository.dart
@@ -1,6 +1,12 @@
 import 'package:fifa_world_cup_app/app/models/groups_stickers.dart';
+import 'package:fifa_world_cup_app/app/models/register_sticker_model.dart';
+import 'package:fifa_world_cup_app/app/models/sticker_model.dart';
 
 abstract class IStickersRepository {
 
   Future<List<GroupsStickers>> getMyAlbum();
+
+  Future<StickerModel?> findStickerByCode(String stickerCode, String stickerNumber);
+
+  Future<StickerModel> create(RegisterStickerModel registerStickerModel);
 }
--- lib/app/repositories/stickers/stickers_repository.dart
+++ lib/app/repositories/stickers/stickers_repository.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:fifa_world_cup_app/app/core/exceptions/repository_exception.dart';
 import 'package:fifa_world_cup_app/app/core/rest/custom_dio.dart';
 import 'package:fifa_world_cup_app/app/models/groups_stickers.dart';
+import 'package:fifa_world_cup_app/app/models/register_sticker_model.dart';
+import 'package:fifa_world_cup_app/app/models/sticker_model.dart';
 import 'package:fifa_world_cup_app/app/repositories/stickers/i_stickers_repository.dart';
 
 class StickersRepository implements IStickersRepository {
@@ -33,6 +35,53 @@ class StickersRepository implements IStickersRepository {
 
       log(errorMessage, error: e, stackTrace: s);
 
+      throw RepositoryException(message: errorMessage);
+    }
+  }
+
+  @override
+  Future<StickerModel?> findStickerByCode(String stickerCode, String stickerNumber) async {
+    
+    try {
+      final result = await _dio.get("/api/sticker-search", data: {
+        "sticker_code": stickerCode,
+        "sticker_number": stickerNumber,
+      });
+
+      return StickerModel.fromMap(result.data);
+    } on DioException catch (e, s) {
+
+      // Esse status seria uma resposta normal do meu sistema, por isso não retorno uma exceção
+      if(e.response?.statusCode == 404) {
+        return null;
+      }
+
+      const errorMessage = "Figurinha não foi encontrada";
+
+      log(errorMessage, error: e, stackTrace: s);
+
+      throw RepositoryException(message: errorMessage);
+    }
+  }
+
+  @override
+  Future<StickerModel> create(RegisterStickerModel registerStickerModel) async {
+    
+    try {
+      final body = FormData.fromMap({
+        ...registerStickerModel.toMap(),
+        "sticker_image_upload": "",
+      });
+
+      final result = await _dio.auth().post("/api/sticker", data: body);
+
+      return StickerModel.fromMap(result.data);
+    } on DioException catch (e, s) {
+
+      const errorMessage = "Erro ao registrar figurinha no álbum";
+
+      log(errorMessage, error: e, stackTrace: s);
+      
       throw RepositoryException(message: errorMessage);
     }
   }
